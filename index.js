47c5de6966052bc51e973f302cd32f27d916a578
Mahfuz <mahfuzru5634@gmail.com>
2025-12-11 00:17:44 +0600

    connected with mongodb and create basic api route for all loan

--- index.js
+++ index.js
@@ -1,13 +1,65 @@
-const express = require('express')
-const app =express();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const dotenv = require("dotenv");
+const port = process.env.PORT || 3000;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
+app.use(cors());
+app.use(express.json());
+dotenv.config();
 
+const uri = process.env.MONGO_URI;
 
-app.get('/',(req,res)=>{
-    res.send("server is runnig..")
-})
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
-const port=3000;
-app.listen(port,()=>{
-    console.log(`server runnig on port ${port}`);
+async function run() {
+  try {
+    await client.connect();
+
+    const db = client.db("microcredx");
+    const allloan = db.collection("allloan");
+
+    //get 6 loan
+    app.get("/home-loans", async (req, res) => {
+      try {
+        const result = await allloan
+          .find({ showOnHome: true })
+          .limit(6)
+          .toArray();
+
+        res.send({
+          status: "success",
+          count: result.length,
+          data: result,
+        });
+      } catch (error) {
+        console.error("Error fetching home loans:", error);
+        res
+          .status(500)
+          .send({ status: "error", message: "Internal Server Error" });
+      }
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("server is runnig..");
+});
+
+app.listen(port, () => {
+  console.log(`server runnig on port ${port}`);
 });
